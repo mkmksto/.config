22cce55cfacaeb8db6cd1819f0f9d63c80bfa106
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-01-11 18:13:19 +0800

    moved lsp files

--- nvim/lua/lemongrass/init.lua
+++ nvim/lua/lemongrass/init.lua
@@ -2,6 +2,10 @@ require("lemongrass.settings")
 require("lemongrass.remap")
 require("lemongrass.plugins")
 require("lemongrass.colorscheme")
+require("lemongrass.lsp.mason")
+require("lemongrass.lsp.lspsaga")
+require("lemongrass.lsp.lspconfig")
+require("lemongrass.lsp.null-ls")
 
 local has = vim.fn.has
 local is_linux = has("unix")
