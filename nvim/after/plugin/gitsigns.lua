33e0e9eaecf5c8dd6756c82a30326f14f6a43812
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-01-16 21:43:54 +0800

    hunks remap

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -16,9 +16,9 @@ gitsigns.setup({
         end
 
         -- Navigation
-        map("n", "]c", function()
+        map("n", "]h", function()
             if vim.wo.diff then
-                return "]c"
+                return "]h"
             end
             vim.schedule(function()
                 gs.next_hunk()
@@ -26,9 +26,9 @@ gitsigns.setup({
             return "<Ignore>"
         end, { expr = true })
 
-        map("n", "[c", function()
+        map("n", "[h", function()
             if vim.wo.diff then
-                return "[c"
+                return "[h"
             end
             vim.schedule(function()
                 gs.prev_hunk()
