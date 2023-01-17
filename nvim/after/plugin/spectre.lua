c8bce619549ad61516994880c8d8f1bc438ae5eb
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-01-17 14:12:09 +0800

    spectre tests

--- nvim/after/plugin/spectre.lua
+++ nvim/after/plugin/spectre.lua
@@ -3,26 +3,13 @@ if not spectre_setup then
     return
 end
 
-spectre.setup({
-    replace_engine = {
-        ["sed"] = {
-            cmd = "sed",
-            args = nil,
-            options = {
-                ["ignore-case"] = {
-                    value = "--ignore-case",
-                    icon = "[I]",
-                    desc = "ignore case",
-                },
-            },
-        },
-    },
-})
+spectre.setup()
 
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 keymap.set("n", "<leader>S", "<cmd>lua require('spectre').open()<CR>", opts)
 keymap.set({ "n" }, "<leader>sw", "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", opts)
 
--- testing spectre hahahahx
+-- testing spectre xixihahx
 -- hahahahx
+-- haha ha haha
