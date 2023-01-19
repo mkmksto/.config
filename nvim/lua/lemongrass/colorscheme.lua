e88ce906f30f295083f2f02c0a49b22df308281e
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-01-19 13:31:55 +0800

    style: changed BG color

--- nvim/lua/lemongrass/colorscheme.lua
+++ nvim/lua/lemongrass/colorscheme.lua
@@ -4,3 +4,8 @@ if not status then
     print("Colorscheme not found")
     return
 end
+
+vim.cmd([[hi Normal guibg=#211c3b]])
+vim.cmd([[hi SignColumn guibg=#211c3b]])
+-- https://stackoverflow.com/questions/250652/vim-initial-settings-change-background-color-for-line-number-on-the-left-side
+vim.cmd([[hi LineNr guibg=#211c3b]])
