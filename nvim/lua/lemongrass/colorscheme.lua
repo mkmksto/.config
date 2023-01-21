d4c7cf5a6b9d6458c20f84ddd2d7d759c54d0174
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-01-21 17:37:06 +0800

    docs

--- nvim/lua/lemongrass/colorscheme.lua
+++ nvim/lua/lemongrass/colorscheme.lua
@@ -5,10 +5,10 @@ if not status then
     return
 end
 
-vim.cmd([[hi Normal guibg=#1b1730]])
-vim.cmd([[hi SignColumn guibg=#1b1730]])
+vim.cmd([[hi Normal guibg=#1b1730]]) -- global BG color?
+vim.cmd([[hi SignColumn guibg=#1b1730]]) -- gutter (i think)
 -- https://stackoverflow.com/questions/250652/vim-initial-settings-change-background-color-for-line-number-on-the-left-side
-vim.cmd([[hi LineNr guibg=#1b1730 ctermbg=DarkGreen]])
+vim.cmd([[hi LineNr guibg=#1b1730 ctermbg=DarkGreen]]) -- numbers at gutter
 
 -- vim.cmd([[hi TabLineFill guibg=#ffffff ctermbg=DarkGreen]])
 -- vim.cmd([[hi TabLine guibg=#ffffff]]) -- probably the entire tabline bar
