6d1a1f1fa4c7347c6aeacd6a744eac45ca89608b
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-01-29 12:52:42 +0800

    fix: comment.nvim supports <C-/> now

--- nvim/after/plugin/comment.lua
+++ nvim/after/plugin/comment.lua
@@ -1,23 +1,26 @@
--- -- -- https://youtu.be/vdn_pKJUda8?list=PLTqGJvc0HUn2GY0sW2L61lDuBaXBKnorO&t=1432
--- local setup, comment = pcall(require, "Comment")
--- if not setup then
---     return
--- end
-
--- comment.setup({
---     toggler = {
---         line = "<C-/>",
---         -- block = "gc",
---     },
--- })
-
--- https://youtu.be/vdn_pKJUda8?list=PLTqGJvc0HUn2GY0sW2L61lDuBaXBKnorO&t=1432
-local nvim_comment_setup, nvim_comment = pcall(require, "nvim_comment")
-if not nvim_comment_setup then
+-- -- https://youtu.be/vdn_pKJUda8?list=PLTqGJvc0HUn2GY0sW2L61lDuBaXBKnorO&t=1432
+local setup, comment = pcall(require, "Comment")
+if not setup then
     return
 end
 
-nvim_comment.setup({
-    -- line_mapping = "<leader>/",
-    operator_mapping = "<C-/>",
+comment.setup({
+    toggler = {
+        line = "<C-/>",
+    },
+    opleader = {
+        line = "<C-/>",
+        block = "gb",
+    },
 })
+
+-- -- https://youtu.be/vdn_pKJUda8?list=PLTqGJvc0HUn2GY0sW2L61lDuBaXBKnorO&t=1432
+-- local nvim_comment_setup, nvim_comment = pcall(require, "nvim_comment")
+-- if not nvim_comment_setup then
+--     return
+-- end
+--
+-- nvim_comment.setup({
+--     -- line_mapping = "<leader>/",
+--     operator_mapping = "<C-/>",
+-- })
