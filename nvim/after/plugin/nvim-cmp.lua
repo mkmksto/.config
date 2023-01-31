42e94da70cc29250836f388a4c7aba5354fefc7a
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-01-31 11:44:12 +0800

    save cmp changes

--- nvim/after/plugin/nvim-cmp.lua
+++ nvim/after/plugin/nvim-cmp.lua
@@ -38,6 +38,16 @@ local function deprioritize_snippet(entry1, entry2)
     end
 end
 
+-- -- https://www.youtube.com/watch?v=z5mqa4ELjhw&list=PLOe6AggsTaVuIXZU4gxWJpIQNHMrDknfN&index=24
+-- -- local kind_mapper = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }
+-- local kind_mapper = cmp_types_config.lsp.CompletionItemKind
+--
+-- local kind_score = {
+--     Class = 1,
+--     Method = 2,
+--     Variable = 3,
+-- }
+
 -- https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources
 cmp.setup({
     -- https://github.com/hrsh7th/nvim-cmp/issues/209
@@ -72,7 +82,7 @@ cmp.setup({
     }),
     -- sources for autocompletion
     sources = cmp.config.sources({
-        { name = "nvim_lsp", max_item_count = 8, priority = 10 }, -- lsp as source for autocompletion
+        { name = "nvim_lsp", max_item_count = 13, priority = 10 }, -- lsp as source for autocompletion
         -- { name = "nvim_lsp", max_item_count = 8, keyword_length = 6 }, -- lsp as source for autocompletion
         { name = "buffer", priority = 8 }, -- text within current buffer
         { name = "path", max_item_count = 8, priority = 7 }, -- file system paths
@@ -84,12 +94,19 @@ cmp.setup({
     sorting = {
         priority_weight = 1.0,
         comparators = {
-            deprioritize_snippet,
-            cmp.config.compare.locality,
-            cmp.config.compare.recently_used,
-            cmp.config.compare.score, -- based on :  score = score + ((#sources - (source_index - 1)) * sorting.priority_weight)
-            cmp.config.compare.offset,
             cmp.config.compare.exact,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
+            -- function(entry1, entry2)
+            --     local kind1 = kind_score[kind_mapper[entry1:get_kind()]] or 50
+            --     local kind2 = kind_score[kind_mapper[entry2:get_kind()]] or 50
+            --     if kind1 < kind2 then
+            --         return true
+            --     end
+            -- end,
+            cmp.config.compare.score, -- based on :  score = score + ((#sources - (source_index - 1)) * sorting.priority_weight)
+            deprioritize_snippet,
+            cmp.config.compare.offset,
             cmp.config.compare.scopes,
             cmp.config.compare.kind,
             cmp.config.compare.sort_text,
