20a7a8fa5dbef4124b0b3fbe996f40f5524b04c7
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-02 13:08:10 +0800

    DAP: better breakpoint icons

--- nvim/after/plugin/debugging.lua
+++ nvim/after/plugin/debugging.lua
@@ -76,3 +76,24 @@ map("n", "<leader>bgr", "<cmd>NvimTreeToggle<CR> <cmd>lua require'dapui'.toggle(
 -- DAP UI
 
 dap_ui.setup()
+
+-- https://github.com/mfussenegger/nvim-dap/discussions/355
+vim.api.nvim_set_hl(0, "blue", { fg = "#3d59a1" })
+vim.api.nvim_set_hl(0, "green", { fg = "#9ece6a" })
+vim.api.nvim_set_hl(0, "yellow", { fg = "#FFFF00" })
+vim.api.nvim_set_hl(0, "orange", { fg = "#f09000" })
+
+vim.fn.sign_define(
+    "DapBreakpoint",
+    { text = "", texthl = "blue", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+    "DapBreakpointCondition",
+    { text = "ﳁ", texthl = "blue", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+    "DapBreakpointRejected",
+    { text = "", texthl = "orange", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define("DapLogPoint", { text = "", texthl = "yellow", linehl = "DapLogPoint", numhl = "DapLogPoint" })
+vim.fn.sign_define("DapStopped", { text = "", texthl = "green", linehl = "DapStopped", numhl = "DapStopped" })
