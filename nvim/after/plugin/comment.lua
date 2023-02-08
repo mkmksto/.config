efdef542325ab97b22dd9edaeedcf26443216065
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-08 15:45:36 +0800

    cleanup

--- nvim/after/plugin/comment.lua
+++ nvim/after/plugin/comment.lua
@@ -13,14 +13,3 @@ comment.setup({
         block = "gb",
     },
 })
-
--- -- https://youtu.be/vdn_pKJUda8?list=PLTqGJvc0HUn2GY0sW2L61lDuBaXBKnorO&t=1432
--- local nvim_comment_setup, nvim_comment = pcall(require, "nvim_comment")
--- if not nvim_comment_setup then
---     return
--- end
---
--- nvim_comment.setup({
---     -- line_mapping = "<leader>/",
---     operator_mapping = "<C-/>",
--- })
