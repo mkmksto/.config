802c31ad75a34252bc221315d61b494e18251e14
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-08 12:01:37 +0800

    null-ls: fix eslint not working

--- nvim/lua/lemongrass/lsp/null-ls.lua
+++ nvim/lua/lemongrass/lsp/null-ls.lua
@@ -20,17 +20,17 @@ null_ls.setup({
         --  to disable file types use
         --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
         formatting.prettierd, -- js/ts formatter
-        -- formatting.prettier, -- js/ts formatter
         formatting.stylua, -- lua formatter
         formatting.black,
         formatting.isort,
         diagnostics.pylint,
-        diagnostics.eslint_d.with({ -- js/ts linter
-            -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-            condition = function(utils)
-                return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-            end,
-        }),
+        diagnostics.eslint_d,
+        -- diagnostics.eslint_d.with({ -- js/ts linter
+        --     -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
+        --     condition = function(utils)
+        --         return utils.root_has_file(".eslintrc.cjs") -- change file extension if you use something else
+        --     end,
+        -- }),
     },
     -- configure format on save
     on_attach = function(current_client, bufnr)
