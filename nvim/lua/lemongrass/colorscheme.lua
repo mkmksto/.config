3dad32bbc5ed84c862b8bcf90274aa9f02cf4b28
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-12 22:10:18 +0800

    colorscheme: switch to catp

--- nvim/lua/lemongrass/colorscheme.lua
+++ nvim/lua/lemongrass/colorscheme.lua
@@ -1,20 +1,42 @@
--- https://youtu.be/vdn_pKJUda8?list=PLTqGJvc0HUn2GY0sW2L61lDuBaXBKnorO&t=899
--- local status, _ = pcall(vim.cmd, "colorscheme oxocarbon")
-local status, _ = pcall(vim.cmd, "colorscheme onedark_vivid")
-if not status then
-    print("Colorscheme not found")
-    return
-end
+require("catppuccin").setup({
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 0.25,
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    styles = {
+        comments = { "italic" },
+    },
+})
 
--- local global_bg = #1b1730 -- for oxocarbon
+-- setup must be called before loading
+vim.cmd.colorscheme("catppuccin")
 
-vim.cmd([[hi Normal guibg=#292734]]) -- global BG color?
-vim.cmd([[hi SignColumn guibg=#292734]]) -- gutter (i think)
--- https://stackoverflow.com/questions/250652/vim-initial-settings-change-background-color-for-line-number-on-the-left-side
-vim.cmd([[hi LineNr guibg=#292734 ctermbg=DarkGreen]]) -- numbers at gutter
+-------------------------------------------
+--
+--
+--
+--
+-- OLD CONFIG
 
--- vim.cmd([[hi TabLineFill guibg=#ffffff ctermbg=DarkGreen]])
--- vim.cmd([[hi TabLine guibg=#ffffff]]) -- probably the entire tabline bar
--- vim.cmd([[hi TabLineSel gui=underline guibg=#fcba03 guifg=#fcba03]]) -- active tab
-vim.cmd([[hi TabLine    gui=NONE guibg=#333232]])
--- vim.cmd([[hi Title    gui=NONE guibg=#fcba03 guifg=#ffffff]])
+-- local status, _ = pcall(vim.cmd, "colorscheme catppuccin-macchiato")
+-- if not status then
+--     print("Colorscheme not found")
+--     return
+-- end
+
+-- -- local global_bg = #1b1730 -- for oxocarbon
+--
+-- vim.cmd([[hi Normal guibg=#292734]]) -- global BG color?
+-- vim.cmd([[hi SignColumn guibg=#292734]]) -- gutter (i think)
+-- -- https://stackoverflow.com/questions/250652/vim-initial-settings-change-background-color-for-line-number-on-the-left-side
+-- vim.cmd([[hi LineNr guibg=#292734 ctermbg=DarkGreen]]) -- numbers at gutter
+--
+-- -- vim.cmd([[hi TabLineFill guibg=#ffffff ctermbg=DarkGreen]])
+-- -- vim.cmd([[hi TabLine guibg=#ffffff]]) -- probably the entire tabline bar
+-- -- vim.cmd([[hi TabLineSel gui=underline guibg=#fcba03 guifg=#fcba03]]) -- active tab
+-- vim.cmd([[hi TabLine    gui=NONE guibg=#333232]])
+-- -- vim.cmd([[hi Title    gui=NONE guibg=#fcba03 guifg=#ffffff]])
