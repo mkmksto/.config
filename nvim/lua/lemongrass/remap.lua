14f7c877a88b068de901de54079a69dc5cf1f17d
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-12 23:22:06 +0800

    feat: add descriptions to keymaps

--- nvim/lua/lemongrass/remap.lua
+++ nvim/lua/lemongrass/remap.lua
@@ -11,7 +11,7 @@ keymap.set("n", "}", "}zz")
 -- my remaps
 -- keymap.set("i", "<S-space", "<Esc>")
 keymap.set("i", "<C-j>", "<Esc>o")
-keymap.set("n", "<C-Enter>", "o<Esc>")
+keymap.set("n", "<C-Enter>", "o<Esc>", { desc = "Create empty newline" })
 keymap.set({ "i", "n", "v" }, "<C-z>", "")
 keymap.set("n", " ", "")
 
@@ -22,9 +22,9 @@ keymap.set("n", "<Left>", "")
 keymap.set("n", "<Right>", "")
 
 -- Buffers
-keymap.set("n", "<Tab>", "<cmd>bnext<CR>")
-keymap.set("n", "<S-Tab>", "<cmd>bprevious<CR>")
-keymap.set("n", "<leader>bd", "<cmd>Bdelete!<CR>")
+keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Next Buffer" })
+keymap.set("n", "<S-Tab>", "<cmd>bprevious<CR>", { desc = "Previous Buffer" })
+keymap.set("n", "<leader>bd", "<cmd>Bdelete!<CR>", { desc = "[B]uffer - [D]elete from list" })
 -- keymap.set("n", "<leader>bd", "<cmd>bdelete!<CR>")
 
 keymap.set("n", "<C-s>", "<cmd>write<CR>", opts) -- i think this is better, avoids typos
@@ -34,35 +34,35 @@ keymap.set("n", "n", "nzzzv")
 keymap.set("n", "N", "Nzzzv")
 
 -- Resize window
-keymap.set("n", "<C-left>", "<C-w>10<")
-keymap.set("n", "<C-right>", "<C-w>10>")
-keymap.set("n", "<C-up>", "<C-w>6+")
-keymap.set("n", "<C-down>", "<C-w>6-")
+keymap.set("n", "<C-left>", "<C-w>10<", { desc = "Resize window" })
+keymap.set("n", "<C-right>", "<C-w>10>", { desc = "Resize window" })
+keymap.set("n", "<C-up>", "<C-w>6+", { desc = "Resize window" })
+keymap.set("n", "<C-down>", "<C-w>6-", { desc = "Resize window" })
 
 -- https://youtu.be/vdn_pKJUda8?list=PLTqGJvc0HUn2GY0sW2L61lDuBaXBKnorO&t=1064
 keymap.set("n", "x", '"_x') -- do not copy a deleted char to register
 
 -- managing tabs
-keymap.set("n", "<leader>tn", ":tabnew<CR>", opts) -- new tab
-keymap.set("n", "<leader>tc", ":tabclose<CR>", opts) -- close tab
-keymap.set("n", "<leader><Tab>", ":tabn<CR>", opts) -- next tab
-keymap.set("n", "<leader><S-Tab>", ":tabp<CR>", opts) -- previous tab
+keymap.set("n", "<leader>tn", ":tabnew<CR>", opts, { desc = "New tab" }) -- new tab
+keymap.set("n", "<leader>tc", ":tabclose<CR>", opts, { desc = "Close tab" }) -- close tab
+keymap.set("n", "<leader><Tab>", ":tabn<CR>", opts, { desc = "Next tab" }) -- next tab
+keymap.set("n", "<leader><S-Tab>", ":tabp<CR>", opts, { desc = "Previous tab" }) -- previous tab
 
 -- nvim tree toglee
-keymap.set("n", "<C-b>", "<cmd>NvimTreeToggle<CR>", opts)
+keymap.set("n", "<C-b>", "<cmd>NvimTreeToggle<CR>", opts, { desc = "Toggle File Explorer(NvimTree)" })
 
 ---------------------------
 --  Telescope
 local builtin = require("telescope.builtin")
 
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find text throughout project
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string the cursor is on throught proj
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Grep inside files" }) -- find text throughout project
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Grep word under cursor" }) -- find string the cursor is on throught proj
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "[Telescope] - List Buffers" })
 
-keymap.set("n", "<leader>thlp", "<cmd>Telescope help_tags<cr>")
-keymap.set("n", "<C-M-p>", "<cmd>Telescope keymaps<cr>")
+keymap.set("n", "<leader>thlp", "<cmd>Telescope help_tags<cr>", { desc = "[Telescope] - show Help Documentation" })
+keymap.set("n", "<C-M-p>", "<cmd>Telescope keymaps<cr>", { desc = "[Telescope] - show all Keymaps" })
 
-keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>")
+keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>", { desc = "[Telescope] - Find Files" })
 
 -- file search over python docs (each file is a text file/python builtin std lib)
 keymap.set("n", "<leader>pdoc", function()
@@ -71,9 +71,8 @@ keymap.set("n", "<leader>pdoc", function()
             "~/.config/telescope_programming_docs/library_python/",
             "~/.config/telescope_programming_docs/python3-2022/",
         },
-        path_display = { "truncate" },
     })
-end)
+end, { desc = "Search for python modules (kw=docs/documentation)" })
 
 -- file search over python docs (each file is a text file/python builtin std lib)
 -- https://terokarvinen.com/2022/ks-kanasirja-offline-tui-dictionary/
@@ -81,19 +80,29 @@ keymap.set("n", "<leader>jdoc", function()
     builtin.find_files({
         search_dirs = { "~/.config/telescope_programming_docs/mdn-2022/" },
     })
-end)
+end, { desc = "Search for JS/MDN modules/functions/global builtins (kw=docs/documentation)" })
 
 -- file search over TLDR
 keymap.set("n", "<leader>tldr", function()
     builtin.find_files({
         search_dirs = { "~/.config/telescope_programming_docs/tldr-2022/" },
     })
-end)
+end, { desc = "TLDR docs(terminal, bash, linux, mac, windows)" })
 
-keymap.set("n", "<leader>gf", "<cmd>Telescope git_files<CR>")
-keymap.set("n", "<leader>tman", "<cmd>Telescope man_pages<CR>") -- linux man pages
-keymap.set("n", "<leader>tgs", "<cmd>Telescope git_status<CR>")
-keymap.set("n", "<leader>thelp", "<cmd>Telescope help_tags<CR>")
+keymap.set("n", "<leader>gf", "<cmd>Telescope git_files<CR>", { desc = "[Telescope] git files" })
+keymap.set(
+    "n",
+    "<leader>tman",
+    "<cmd>Telescope man_pages<CR>",
+    { desc = "[Telescope] Search through the linux man pages" }
+) -- linux man pages
+keymap.set("n", "<leader>tgs", "<cmd>Telescope git_status<CR>", { desc = "[Telescope] git status" })
+keymap.set(
+    "n",
+    "<leader>thelp",
+    "<cmd>Telescope help_tags<CR>",
+    { desc = "[Telescope] Search through telescope's help manual " }
+)
 
 -- Telescope with rg syntax
 local telescope_rg = require("telescope").extensions.live_grep_args
@@ -103,39 +112,51 @@ keymap.set("n", "<leader>trg", function()
         additional_args = { "*.md" },
         -- default_text = "^ ~/Documents/Github-repos/*",
     })
-end)
+end, { desc = "[Telescope] live grep with args (defaults to cwd)" })
 
 -- keymap.set("n", "<leader>trg", "<cmd>Telescope live_grep_args<CR>")
 -- keymap.set("n", "<leader>trg", '<cmd>Telescope live_grep_args search_dirs={"~/Documents/Github-repos/"}<CR>')
 
 -- Telescope LSP stuff
-keymap.set("n", "<leader>tgf", "<cmd>Telescope lsp_references<CR>")
-keymap.set("n", "<leader>tgi", "<cmd>Telescope lsp_implementations<CR>")
-keymap.set("n", "<leader>tgd", "<cmd>Telescope lsp_definitions<CR>")
-keymap.set("n", "<leader>tT", "<cmd>Telescope lsp_type_definitions<CR>")
+keymap.set("n", "<leader>tgf", "<cmd>Telescope lsp_references<CR>", { desc = "[Telescope] LSP References" })
+keymap.set("n", "<leader>tgi", "<cmd>Telescope lsp_implementations<CR>", { desc = "[Telescope] LSP Implementations" })
+keymap.set("n", "<leader>tgd", "<cmd>Telescope lsp_definitions<CR>", { desc = "[Telescope] LSP Definitions" })
+keymap.set("n", "<leader>tT", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "[Telescope] LSP Type Definition" })
 
 ---------------------------
 --
 
 -- Move text up and down (https://github.com/LunarVim/LunarVim/blob/master/lua/lvim/keymappings.lua)
-keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
-keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
-keymap.set("n", "<A-j>", ":m .+1<CR>==")
-keymap.set("n", "<A-k>", ":m .-2<CR>==")
+keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "swap lines" })
+keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "swap lines" })
+keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "swap lines" })
+keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "swap lines" })
 
 -- pasting stuff (from prime's vid) (https://youtu.be/w7i4amO_zaE?t=1610)
-keymap.set("x", "<leader>p", '"_dP')
-keymap.set("n", "Q", "<nop>")
+keymap.set("x", "<leader>p", '"_dP', { desc = "Do not copy singly deleted characters to register" })
+keymap.set("n", "Q", "<nop>", { desc = "they said S-q is dangerous, dunno why" })
 
 -- visual line comments
 -- keymap.set({ "n", "o" }, "<C-/>", "<cmd>CommentToggle<CR>")
 
 -- docstrings
-keymap.set({ "n" }, "<leader>dpy", "<cmd>DogeGenerate google<CR>", opts)
-keymap.set({ "n" }, "<leader>djs", "<cmd>DogeGenerate jsdoc<CR>", opts)
+keymap.set(
+    { "n" },
+    "<leader>dpy",
+    "<cmd>DogeGenerate google<CR>",
+    { desc = "[Doge] Generate Python docs docstrings in google standard" },
+    opts
+)
+keymap.set({ "n" }, "<leader>djs", "<cmd>DogeGenerate jsdoc<CR>", { desc = "[Doge] Generate JSdoc docstrings" }, opts)
 
 -- toggles markdown checkboxes
-keymap.set("n", "<leader>tt", "<cmd>lua require('toggle-checkbox').toggle()<CR>", opts)
+keymap.set(
+    "n",
+    "<leader>tt",
+    "<cmd>lua require('toggle-checkbox').toggle()<CR>",
+    { desc = "Toggle markdown checkboxes" },
+    opts
+)
 
 -- -- https://github.com/jghauser/follow-md-links.nvim
 -- keymap.set("n", "<bs>", ":edit #<cr>", { silent = true })
