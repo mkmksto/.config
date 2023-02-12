42556c7f42ad82e3224fdcb5b7adfb444167e710
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-12 23:38:38 +0800

    toggleterm: keymap descriptions

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -25,7 +25,12 @@ end
 
 -- https://stackoverflow.com/questions/13511084/vim-set-cursor-position-in-command-line
 -- vim.keymap.set("n", "<leader>`", ":ToggleTerm direction=horizontal auto_scroll=true<C-Left><C-Left><Left>") -- unnecessary, just to 2<C-\>, etc.
-vim.keymap.set({ "n", "t" }, "<C-`>", "<Esc><C-\\><C-n><cmd>ToggleTermToggleAll<CR>")
+vim.keymap.set(
+    { "n", "t" },
+    "<C-`>",
+    "<Esc><C-\\><C-n><cmd>ToggleTermToggleAll<CR>",
+    { desc = "[Toggleterm] Toggle all terminals" }
+)
 -- vim.keymap.set("n", "<leader>lz", ":LazyGit<CR>")
 
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
@@ -38,28 +43,43 @@ local python = Terminal:new({ cmd = "python3", hidden = true, direction = "float
 function _PYTHON_TOGGLE()
     python:toggle()
 end
-vim.keymap.set("n", "<leader>tpy", ":lua _PYTHON_TOGGLE()<CR>")
+vim.keymap.set(
+    "n",
+    "<leader>tpy",
+    ":lua _PYTHON_TOGGLE()<CR>",
+    { desc = "[Toggleterm] Toggle Python floating terminal" }
+)
 
 local node_js = Terminal:new({ cmd = "node", hidden = true, direction = "float", count = 7 })
 function _NODE_TOGGLE()
     node_js:toggle()
 end
-vim.keymap.set("n", "<leader>tjs", ":lua _NODE_TOGGLE()<CR>")
+vim.keymap.set(
+    "n",
+    "<leader>tjs",
+    ":lua _NODE_TOGGLE()<CR>",
+    { desc = "[Toggleterm] Toggle NODE JS floating terminal" }
+)
 
 local floating_term = Terminal:new({ hidden = true, direction = "float", close_on_exit = true, count = 5 })
 function _FLOATING_TOGGLE()
     floating_term:toggle()
 end
-vim.keymap.set("n", "<leader>fl", ":lua _FLOATING_TOGGLE()<CR>")
+vim.keymap.set("n", "<leader>fl", ":lua _FLOATING_TOGGLE()<CR>", { desc = "[Toggleterm] Toggle floating terminal" })
 
 local htop = Terminal:new({ cmd = "htop", hidden = true, direction = "float", count = 8 })
 function _HTOP_TOGGLE()
     htop:toggle()
 end
-vim.keymap.set("n", "<leader>htop", ":lua _HTOP_TOGGLE()<CR>")
+vim.keymap.set("n", "<leader>htop", ":lua _HTOP_TOGGLE()<CR>", { desc = "[Toggleterm] Toggle floating HTOP terminal" })
 
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float", count = 9 })
 function _LAZYGIT_TOGGLE()
     lazygit:toggle()
 end
-vim.keymap.set("n", "<leader>lz", ":lua _LAZYGIT_TOGGLE()<CR>")
+vim.keymap.set(
+    "n",
+    "<leader>lz",
+    ":lua _LAZYGIT_TOGGLE()<CR>",
+    { desc = "[Toggleterm] Toggle floating Lazygit terminal" }
+)
