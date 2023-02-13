d40484bf27fd9301cdaeda4661d0c133d9909942
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-13 22:50:40 +0800

    cleanup

--- nvim/after/plugin/neoclip.lua
+++ nvim/after/plugin/neoclip.lua
@@ -1,9 +1,7 @@
 require("neoclip").setup({
     default_register = { "+", '"', "*" },
-    enable_macro_history = true,
     on_select = {
         move_to_front = true,
-        -- close_telescope = true,
     },
     keys = {
         telescope = {
