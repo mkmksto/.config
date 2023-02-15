14a55d91e7eae75a1de427b306d161a744711e8e
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-15 19:37:03 +0800

    markdown preview: browser support for WSL

--- nvim/after/plugin/markdown-preview.lua
+++ nvim/after/plugin/markdown-preview.lua
@@ -1,2 +1,17 @@
-vim.g.mkdp_browser = "/usr/bin/epiphany"
-vim.g.mkdp_theme = "light"
+local has = vim.fn.has
+local is_linux = has("unix")
+
+if is_linux then
+    -- TODO: check if this works
+    vim.g.mkdp_browser = "/usr/bin/epiphany"
+end
+
+-- https://www.reddit.com/r/neovim/comments/vr68yl/checking_for_wsl_in_initlua/
+local is_wsl = (function()
+    local output = vim.fn.systemlist("uname -r")
+    return not not string.find(output[1] or "", "WSL")
+end)()
+
+if is_wsl then
+    vim.g.mkdp_browser = "C:/Program Files/Mozilla Firefox/firefox.exe"
+end
