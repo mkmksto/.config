0a6248b2d34b566ac89af968852e624bf9a439f9
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-15 23:04:16 +0800

    markdown preview light

--- nvim/after/plugin/markdown-preview.lua
+++ nvim/after/plugin/markdown-preview.lua
@@ -4,6 +4,7 @@ local is_linux = has("unix")
 if is_linux then
     -- TODO: check if this works
     vim.g.mkdp_browser = "/usr/bin/epiphany"
+    vim.g.mkdp_theme = "light"
 end
 
 -- https://www.reddit.com/r/neovim/comments/vr68yl/checking_for_wsl_in_initlua/
@@ -14,4 +15,5 @@ end)()
 
 if is_wsl then
     vim.g.mkdp_browser = "C:/Program Files/Mozilla Firefox/firefox.exe"
+    vim.g.mkdp_theme = "light"
 end
