4b3167a114d445c5766f9479a10324af26de4d7e
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-15 11:38:44 +0800

    packer bootstrap

--- nvim/lua/lemongrass/plugins.lua
+++ nvim/lua/lemongrass/plugins.lua
@@ -1,4 +1,21 @@
-local packer = require("packer")
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+local status, packer = pcall(require,"packer")
+if not status then
+	return
+end
+
 packer.init({
     snapshot_path = vim.fn.expand("~/.config/nvim/packer_snaps/"),
 })
