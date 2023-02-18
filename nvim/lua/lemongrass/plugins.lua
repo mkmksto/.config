77262328dbbdba54f25ac8a0c852ce686e72c035
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-18 20:48:43 +0800

    new plug: tabline

--- nvim/lua/lemongrass/plugins.lua
+++ nvim/lua/lemongrass/plugins.lua
@@ -71,11 +71,40 @@ return require("packer").startup(function(use)
     })
     use({ "tpope/vim-surround", lock = true })
 
-    -- file explorer, file operations, buffer operations
+    -- file explorer, file operations, buffer operations, tabs
     use({ "nvim-tree/nvim-tree.lua", lock = true })
     use({ "kyazdani42/nvim-web-devicons", lock = true })
-    use({ "nvim-lualine/lualine.nvim", lock = true })
-    use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons", lock = true })
+    use({ "nvim-lualine/lualine.nvim", lock = true }) -- https://github.com/Strazil001/Nvim/blob/main/lua/svdv/packer.lua
+    -- use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons", lock = true })
+    use({
+        "kdheepak/tabline.nvim",
+        config = function()
+            require("tabline").setup({
+                -- Defaults configuration options
+                enable = true,
+                options = {
+                    -- If lualine is installed tabline will use separators configured in lualine by default.
+                    -- These options can be used to override those settings.
+                    component_separators = { "", "" },
+                    section_separators = { "", "" },
+                    max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
+                    show_tabs_always = true, -- this shows tabs only when there are more than one tab or if the first tab is named
+                    show_devicons = true, -- this shows devicons in buffer section
+                    colored = true,
+                    show_bufnr = false, -- this appends [bufnr] to buffer section,
+                    tabline_show_last_separator = true,
+                    show_filename_only = true, -- shows base filename only instead of relative path in filename
+                    modified_icon = "+ ", -- change the default modified icon
+                    modified_italic = true, -- set to true by default; this determines whether the filename turns italic if modified
+                    show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
+                },
+            })
+            vim.cmd([[
+              set guioptions-=e " Use showtabline in gui vim
+              set sessionoptions+=tabpages,globals " store tabpages and globals in session
+            ]])
+        end,
+    })
     use({ "moll/vim-bbye", lock = true })
 
     -- aucomplete / cmp / autocomplettion
