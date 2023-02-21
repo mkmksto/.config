d895baeb1a4935bda962120aa573dd75829510d2
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-21 15:18:09 +0800

    feat(linux): use chrome new window for mkdwn prev

--- nvim/lua/lemongrass/lazyload/markdown-preview.lua
+++ nvim/lua/lemongrass/lazyload/markdown-preview.lua
@@ -2,9 +2,18 @@ local has = vim.fn.has
 local is_linux = has("unix")
 
 if is_linux then
+    -- https://github.com/iamcco/markdown-preview.nvim/issues/19
+    -- https://github.com/iamcco/markdown-preview.nvim/issues/262
+    vim.cmd([[
+        function OpenMarkdownPreview (url)
+          silent exec "!google-chrome --new-window " . a:url . " &"
+        endfunction
+    ]])
     -- TODO: check if this works
-    vim.g.mkdp_browser = "/usr/bin/epiphany"
+    -- vim.g.mkdp_browser = "/usr/bin/epiphany"
+    vim.g.mkdp_browser = "google-chrome"
     vim.g.mkdp_theme = "light"
+    vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
 end
 
 -- https://www.reddit.com/r/neovim/comments/vr68yl/checking_for_wsl_in_initlua/
