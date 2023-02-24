403dbebb9c5d4407283493e4084d0c958136a0ba
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-24 12:42:16 +0800

    open chrome for wsl

--- nvim/lua/lemongrass/lazyload/markdown-preview.lua
+++ nvim/lua/lemongrass/lazyload/markdown-preview.lua
@@ -1,7 +1,21 @@
 local has = vim.fn.has
 local is_linux = has("unix")
+-- https://www.reddit.com/r/neovim/comments/vr68yl/checking_for_wsl_in_initlua/
+local is_wsl = (function()
+    local output = vim.fn.systemlist("uname -r")
+    return not not string.find(output[1] or "", "WSL")
+end)()
 
-if is_linux then
+if is_wsl then
+    vim.cmd([[
+        function OpenMarkdownPreview (url)
+          silent exec '!"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe" --new-window ' . a:url . " &"
+        endfunction
+    ]])
+    -- vim.g.mkdp_browser = "C:/Program Files/Mozilla Firefox/firefox.exe"
+    vim.g.mkdp_browser = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+    vim.g.mkdp_theme = "light"
+elseif is_linux then
     -- https://github.com/iamcco/markdown-preview.nvim/issues/19
     -- https://github.com/iamcco/markdown-preview.nvim/issues/262
     vim.cmd([[
@@ -14,15 +28,4 @@ if is_linux then
     vim.g.mkdp_browser = "google-chrome"
     vim.g.mkdp_theme = "light"
     vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
-end
-
--- https://www.reddit.com/r/neovim/comments/vr68yl/checking_for_wsl_in_initlua/
-local is_wsl = (function()
-    local output = vim.fn.systemlist("uname -r")
-    return not not string.find(output[1] or "", "WSL")
-end)()
-
-if is_wsl then
-    vim.g.mkdp_browser = "C:/Program Files/Mozilla Firefox/firefox.exe"
-    vim.g.mkdp_theme = "light"
 end
