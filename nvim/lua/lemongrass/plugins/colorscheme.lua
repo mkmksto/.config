fb26143a2666456cb8fc88ca58c05f400404b2db
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-09-09 16:23:15 +0800

    ui: switch theme to catpuccin

--- nvim/lua/lemongrass/plugins/colorscheme.lua
+++ nvim/lua/lemongrass/plugins/colorscheme.lua
@@ -1,7 +1,9 @@
 return {
-    "Shatur/neovim-ayu",
+    -- "Shatur/neovim-ayu",
+    "catppuccin/nvim",
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme("ayu-mirage")
+        -- vim.cmd.colorscheme("ayu-mirage")
+        vim.cmd.colorscheme("catppuccin-macchiato")
     end,
 }
